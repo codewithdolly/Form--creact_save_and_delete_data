1513ff9c39ca3e7a67629a8d9193059ad1019913
hicoderindia <hicoder.dolly@gmail.com>
2022-01-06 22:40:18 +0530

    form data added

--- app.js
+++ app.js
@@ -1,63 +1,82 @@
-const md_url=process.env.md_url;
+const md_url = process.env.md_url;
+const PORT = 4000;
 
-const express = require('express')
-const ejs=require('ejs')
-const bodyParser=require('body-parser')
-const mongoose = require('mongoose');
-
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 //Express App
-const app = express()
+const app = express();
 // create application/x-www-form-urlencoded parser
-app.use(bodyParser.urlencoded({extended:false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //connecting mongoose
-await mongoose.connect(md_url)
-
+mongoose.connect(
+  "mongodb+srv://heydollysingh:cdufp8mviyB7Lmk@cluster0.vfyvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
 
 // Home - render the form
-app.get('/', function (req, res) {
-    res.render("form.ejs")
-})
+app.get("/", function (req, res) {
+  res.render("form.ejs");
+});
+
+
 
 
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const Cord = new Schema({
+const projectCoordinate = new Schema({
   id: ObjectId,
   title: String,
   coords: String,
-  shape: String
+  shape: String,
+});
+
+//Creating Model
+const Coordinate = mongoose.model("Coordinate", projectCoordinate);
+// create a coordinate post
+var codata = new Coordinate();
+
+// receiving data from submitted form
+app.post("/submit-data", function (req, res) {
+  //print body
+  console.log(req.body);
+
+  //save in db
+  codata = req.body;
+
+  Coordinate.create(codata, (err, createdData) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("data inserted successfully" + createdData);
+      res.redirect('/data'); //send a respose  that data is saved in DB
+    }
+  });
 });
 
 
 
+app.get("/data",(req,res)=>{
 
-// receiving data from submitted form
-app.post('/submit-data', function (req,res) {
+Coordinate.find((err,Coordinates)=>{
 
-//print body
-console.log(req.body)
+if(!err){
+  console.log("data found"+Coordinates)
+res.render("data.ejs",{Coordinates:Coordinates})
 
-//save in db
+}
+else{
+  console.log(err)
+}
 
-
-
-//send a respose  that data is saved in DB
-res.send('welcome, ' + req.body.title)
+})
+  
 })
 
- 
 
 
-
-
-
-
-
-
-
-
-app.listen(4000, console.log("server started on port 4000"))
+app.listen(PORT, console.log("server started on port " + PORT));
